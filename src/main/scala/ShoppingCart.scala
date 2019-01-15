7d9b3cf73f7c19712ca4e10b930ce9ce8ad28e45
EbaiNsoatabe <ebainsoatabe@googlemail.com>
2019-01-15 11:07:29 +0000

    first step

--- src/main/scala/ShoppingCart.scala
+++ src/main/scala/ShoppingCart.scala
@@ -3,11 +3,11 @@ object ShoppingCart extends App{
   var orange = 0
 
   def checkOut(apples:Int, oranges:Int){
-    var cost = apples*0.6 + oranges*0.25
+    val cost = apples*0.6 + oranges*0.25
     println(f"Total cost is £$cost%2.2f")
   }
 
-  val scannedItems = List(1, 1, 2, 1, 2, 1)
+  val scannedItems = List(2, 2, 2, 2, 2)
 
   for(i <- 0 until scannedItems.length){
     if(scannedItems(i) == 1){
@@ -18,5 +18,27 @@ object ShoppingCart extends App{
     }
   }
 
+  def appleDiscounts(): Unit ={
+    if(apple % 2 == 0){
+      apple = apple/2
+    }
+    else{
+      apple = 1 + (apple - 1)/2
+    }
+  }
+
+  def orangeDiscounts(): Unit ={
+    if(orange % 3 == 0){
+      orange = (orange/3)*2
+    }
+    else if(orange % 3 == 1){
+      orange = 1 + ((orange - 1)/3)*2
+    }
+    else{
+      orange = 2 + ((orange - 2)/3)*2
+    }
+  }
+  appleDiscounts()
+  orangeDiscounts()
   checkOut(apple, orange)
 }
