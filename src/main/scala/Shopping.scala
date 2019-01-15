22ed135bf995cf64a9c6e5c3f09f5688e919cb04
EbaiNsoatabe <ebainsoatabe@googlemail.com>
2019-01-15 11:21:12 +0000

    first step

---
+++ src/main/scala/Shopping.scala
@@ -0,0 +1,22 @@
+object Shopping extends App{
+  var apple = 0
+  var orange = 0
+
+  def checkOut(apples:Int, oranges:Int){
+    var cost = apples*0.6 + oranges*0.25
+    println(f"Total cost is £$cost%2.2f")
+  }
+
+  val scannedItems = List(1, 1, 2, 1, 2, 1)
+
+  for(i <- 0 until scannedItems.length){
+    if(scannedItems(i) == 1){
+      apple += 1
+    }
+    else{
+      orange += 1
+    }
+  }
+
+  checkOut(apple, orange)
+}
